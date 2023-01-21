99505bbd456e8e1d7007257312991f83ac12461f
yuelimin <yueliminvc@outlook.com>
2023-01-21 23:38:40 +0800

    development/transaction-manager:init

---
+++ driving-transaction-manager/src/main/java/com/driving/transaction/TransactionManagerApplication.java
@@ -0,0 +1,16 @@
+package com.driving.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yuelimin
+ * @version 1.0.0
+ * @since 11
+ */
+@SpringBootApplication
+public class TransactionManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionManagerApplication.class, args);
+    }
+}
