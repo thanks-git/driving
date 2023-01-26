b92274d2f494f3499c91f4a9c7860b61dd107762
yuelimin <yueliminvc@outlook.com>
2023-01-26 19:37:26 +0800

    development/order:update

--- driving-order/src/main/java/com/driving/order/config/SpringDocConfig.java
+++ driving-order/src/main/java/com/driving/order/config/SpringDocConfig.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "driving-driver", description = "华夏代驾订单子系统", version = "1.0.0"))
+@OpenAPIDefinition(info = @Info(title = "driving-order", description = "华夏代驾订单子系统", version = "1.0.0"))
 public class SpringDocConfig {
     // todo: open-api configuration
 }
