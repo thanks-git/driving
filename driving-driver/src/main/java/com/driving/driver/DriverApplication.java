fa5044d8e30298df3137b5bfd9c404f6aa7eab0a
yuelimin <yueliminvc@outlook.com>
2023-01-26 18:48:50 +0800

    development/driver:update

--- driving-driver/src/main/java/com/driving/driver/DriverApplication.java
+++ driving-driver/src/main/java/com/driving/driver/DriverApplication.java
@@ -1,8 +1,12 @@
 package com.driving.driver;
 
+import com.codingapi.txlcn.tc.config.EnableDistributedTransaction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author yuelimin
@@ -11,6 +15,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients
+@ServletComponentScan
+@ComponentScan("com.driving.*")
+@EnableDistributedTransaction
 public class DriverApplication {
     public static void main(String[] args) {
         SpringApplication.run(DriverApplication.class, args);
