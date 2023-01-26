634180d138719590f8b76af4a6c595f76f638055
yuelimin <yueliminvc@outlook.com>
2023-01-26 19:09:43 +0800

    development/transaction-manager:update

--- driving-transaction-manager/src/main/java/com/driving/transaction/TransactionManagerApplication.java
+++ driving-transaction-manager/src/main/java/com/driving/transaction/TransactionManagerApplication.java
@@ -1,5 +1,6 @@
 package com.driving.transaction;
 
+import com.codingapi.txlcn.tm.config.EnableTransactionManagerServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 11
  */
 @SpringBootApplication
+@EnableTransactionManagerServer
 public class TransactionManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(TransactionManagerApplication.class, args);
