9d5bf9d66df70da6c97d6a39124b75d3cd1a7f43
yuelimin <yueliminvc@outlook.com>
2023-01-26 19:27:12 +0800

    development/order:update

--- driving-order/src/main/java/com/driving/order/OrderApplication.java
+++ driving-order/src/main/java/com/driving/order/OrderApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author YueLiMin
@@ -15,7 +16,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients
 @ServletComponentScan
-//@ComponentScan("com.driving.*")
+@ComponentScan("com.driving.*")
 @EnableDistributedTransaction
 public class OrderApplication {
     public static void main(String[] args) {
