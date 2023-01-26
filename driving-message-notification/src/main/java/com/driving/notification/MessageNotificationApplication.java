7fef0e354efb1c7fa8b551aff78a989758616b4a
yuelimin <yueliminvc@outlook.com>
2023-01-26 23:20:11 +0800

    development/message-notification:update

--- driving-message-notification/src/main/java/com/driving/notification/MessageNotificationApplication.java
+++ driving-message-notification/src/main/java/com/driving/notification/MessageNotificationApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
@@ -15,7 +16,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableDiscoveryClient
 @EnableFeignClients
 @ServletComponentScan
-// @ComponentScan("com.driving.*")
+@ComponentScan("com.driving.*")
 @EnableAsync
 public class MessageNotificationApplication {
     public static void main(String[] args) {
