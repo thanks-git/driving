a25052ea4a304d0f748b86ad2465c3f3a2a9c165
yuelimin <yueliminvc@outlook.com>
2023-01-26 23:46:57 +0800

    development/order-execute:init

---
+++ driving-order-execute/src/main/java/com/driving/execute/OrderExecuteApplication.java
@@ -0,0 +1,25 @@
+package com.driving.execute;
+
+import com.codingapi.txlcn.tc.config.EnableDistributedTransaction;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author YueLiMin
+ * @version 1.0.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@ServletComponentScan
+@ComponentScan("com.driving.*")
+@EnableDistributedTransaction
+public class OrderExecuteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderExecuteApplication.class, args);
+    }
+}
---
+++ driving-order-execute/src/main/java/com/driving/execute/config/SpringDocConfig.java
@@ -0,0 +1,11 @@
+package com.driving.execute.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "driving-order-execute", description = "华夏代驾订单执行子系统", version = "1.0.0"))
+public class SpringDocConfig {
+    // todo: open-api configuration
+}
