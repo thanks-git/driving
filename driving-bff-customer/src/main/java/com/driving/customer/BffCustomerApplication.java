b69b7d7c3390c28a54c894017bef221f10d7d196
yuelimin <yueliminvc@outlook.com>
2023-01-27 22:22:45 +0800

    development/bff-customer:update

--- driving-bff-customer/src/main/java/com/driving/customer/BffCustomerApplication.java
+++ driving-bff-customer/src/main/java/com/driving/customer/BffCustomerApplication.java
@@ -1,8 +1,23 @@
 package com.driving.customer;
 
+import com.codingapi.txlcn.tc.config.EnableDistributedTransaction;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
 /**
  * @author YueLiMin
  * @version 1.0.0
  */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@ComponentScan("com.driving.*")
+@EnableDistributedTransaction
 public class BffCustomerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BffCustomerApplication.class, args);
+    }
 }
